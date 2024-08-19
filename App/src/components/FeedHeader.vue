c55bfb721202086e5b0d1e728acf7eb6afb96a0b
Matheus Sanches <matheus.sanches@ciag.org.br>
2024-08-19 14:54:23 -0300

    add ionic app

---
+++ App/src/components/FeedHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <span>
+      <img src="../../public/assets/LogoEscrita.svg" alt="">
+    </span>
+    <span>
+      <img src="../theme/assets/icons/BellIcon.svg" alt="">
+    </span>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: String,
+});
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 2rem;
+}
+</style>
---
+++ App/src/views/TabsPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="home" href="/tabs/tab1">
+          <ion-icon aria-hidden="true" :icon="home" />
+        </ion-tab-button>
+
+        <ion-tab-button tab="create" href="/tabs/tab2">
+          <ion-icon aria-hidden="true" :icon="addCircleOutline" />
+        </ion-tab-button>
+
+        <ion-tab-button tab="search" href="/tabs/tab3">
+          <ion-icon aria-hidden="true" :icon="searchOutline" />
+        </ion-tab-button>
+
+        <ion-tab-button tab="profile" href="/tabs/tab3">
+          <ion-icon aria-hidden="true" :icon="personCircleOutline" />
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonTabBar, IonTabButton, IonTabs, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import {
+  searchOutline, home, addCircleOutline, personCircleOutline
+  
+ } from 'ionicons/icons';
+</script>
