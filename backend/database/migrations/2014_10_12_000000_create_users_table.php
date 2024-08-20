7d0866d1b92836811cf415161cf7eeed1ad4c22b
Matheus Sanches <matheus.sanches@ciag.org.br>
2024-08-20 20:36:05 -0300

    feat: add services

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('document')->unique();
-            $table->string('zip_code');
             $table->string('phone');
+            $table->string('instagram');
             $table->enum('user_level', ['A','P','C']);
             $table->rememberToken();
             $table->timestamps();
--- backend/database/migrations/2024_04_24_132235_create_animais_table.php
+++ backend/database/migrations/2024_04_24_132235_create_animais_table.php
@@ -16,21 +16,14 @@ class CreateAnimaisTable extends Migration
         Schema::create('animais', function (Blueprint $table) {
             $table->id();
             $table->string('animal_name');
-            $table->integer('age');
-            $table->enum('gender', ['M', 'F']);
             $table->text('description')->nullable();
-            $table->enum('size', ['P','M','G','GG']);
-            $table->float('weight');
-            $table->string('temperament')->nullable();
             $table->unsignedBigInteger('status_id');
             $table->unsignedBigInteger('owner_id');
-            $table->unsignedBigInteger('species_id');
             $table->timestamps();
 
             // Definindo as chaves estrangeiras
             $table->foreign('status_id')->references('id')->on('status_animal');
             $table->foreign('owner_id')->references('id')->on('users');
-            $table->foreign('species_id')->references('id')->on('especies');
         });
     }
 
