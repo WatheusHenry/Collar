37438e63e584e2ff1ec7eda171b2a5377ccf0b06
Murilo Santos <murilo-093@hotmail.com>
2024-09-03 20:44:03 -0300

    Merge

--- App/src/router/index.ts
+++ App/src/router/index.ts
@@ -1,51 +1,48 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
-
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import TabsPage from "../views/TabsPage.vue";
+import Login from "@/views/Login.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/app/home',
+    path: "/",
+    redirect: "/app/home",
   },
   {
-    path: '/app',
-    component:TabsPage,
+    path: "/login",
+    component: Login,
+  },
+  {
+    path: "/app",
+    component: TabsPage,
     children: [
       {
-        path: '/app/home',
-        component: () => import('../views/Feed.vue'),
+        path: "/app/home",
+        component: () => import("../views/Feed.vue"),
       },
       {
         path: "/app/create",
-        component: () => import('../views/CreatePublication.vue')
+        component: () => import("../views/CreatePublication.vue"),
       },
       {
         path: "/app/search",
-        component: () => import('../views/Search.vue')
+        component: () => import("../views/Search.vue"),
       },
       {
         path: "/app/details/:pubId",
-        component: () => import('../views/PublicationDetails.vue')
+        component: () => import("../views/PublicationDetails.vue"),
       },
       {
         path: "/app/profile",
-        component: () => import('../views/Profile.vue')
+        component: () => import("../views/Profile.vue"),
       },
-
-      {
-        path: "/app/login",
-        component: () => import('../views/Login.vue')
-      },
-    
-
-    ]
-  }
-]
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
