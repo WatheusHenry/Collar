1b42066c78e936be6053719674cab09258351796
Matheus Henry <goncalvessanches.mh@gmail.com>
2024-10-15 20:21:15 -0300

    feat

--- App/src/animations/customTransition.ts
+++
@@ -1,9 +0,0 @@
-import { Animation, createAnimation } from '@ionic/core';
-
-export function myCustomAnimation(el: HTMLElement):Animation {
-  return createAnimation()
-    .addElement(el)
-    .duration(500)
-    .fromTo('transform', 'translateX(100%)', 'translateX(0%)')
-    .fromTo('opacity', 0, 1);
-}
--- App/src/utils/auth.ts
+++
@@ -1,4 +0,0 @@
-export function isAuthenticated() {
-  const user = localStorage.getItem('token');  // Verifica se o token está no localStorage
-  return !!user;  // Retorna true se houver um usuário
-}
